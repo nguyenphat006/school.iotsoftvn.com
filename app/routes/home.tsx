cecb2350916dbedf4fb9dd1f3a9c92bc56422ae4
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-12-07 14:39:17 +0700

    .

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,15 +1,19 @@
 import { useState, useEffect } from 'react';
 import type { Route } from './+types/home'
+import { useOutletContext } from 'react-router';
 import { StudentAchievementsSection, LetsBeginCTA, EducationLevel, TheNumbers, TestimonialQuoteSection, EducationPillarsSection, CoreStrengthModal, CoreStrengthsSection, FoundingMessageSection, NewsEventsSection, TestimonialsSection, HeroCarousel, CollegeAcceptancesSection, EducationPillarModal, TypicalDaySection, AcademicSection, StudentCareSection, OverviewSection, GlobalRankingSection } from '@/components/home-page'
 import ScrollToTop from '@/components/ui/components/ScrollToTop';
 import { SolidEducationSection } from '@/components/home-page/section/solid-education';
 import { LHBSLifeVideoSection } from '@/components/home-page/section/life-video';
+
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'LHBS - Trường Song Ngữ Lạc Hồng' }, { name: 'description', content: 'Chào mừng tới Trường Song Ngữ Lạc Hồng - LHBS' }]
 }
 
-export default function Home({onNavigate}: {onNavigate: (path: string) => void}) {
-    return(
+export default function Home() {
+  const { onNavigate } = useOutletContext<{ onNavigate: (path: string) => void }>();
+  
+  return(
     <>
     
       <HeroCarousel onNavigate={onNavigate} />
